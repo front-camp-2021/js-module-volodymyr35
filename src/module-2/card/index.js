4128a084851e68582ddb7c1660a44ac5aa9f605e
volodymyr35 <vzhukevych@gmail.com>
2021-09-25 19:38:18 +0300

    completed js-task Card module-2

--- src/module-2/card/index.js
+++ src/module-2/card/index.js
@@ -1,14 +1,14 @@
 export default class Card {
   element;
 
-  constructor ({
-    id = '',
+  constructor({
+    id = "",
     images = [],
-    title = '',
+    title = "",
     rating = 0,
     price = 0,
-    category = '',
-    brand = ''
+    category = "",
+    brand = "",
   } = {}) {
     this.id = id;
     this.images = images;
@@ -21,7 +21,40 @@ export default class Card {
     this.render();
   }
 
-  render () {
-    // ... your logic
+  render() {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `
+      <div class="card__inner">
+        <div class="card__img">
+          <img src="${this.images[0]}" alt="product" />
+        </div>
+        <div class="card__details">
+          <div class="card__rate-and-price">
+            <button class="card__product--rate">${this.rating}&nbsp;
+              <img class="card__rate" src="./icons/star.svg" alt="">
+            </button>
+            <span class="card__price">₴${this.price}</span>
+          </div>
+          <h4 class="card__title">${this.title}</h4>
+          <p class="card__info">Redesigned from scratch and completely revised.</p>
+        </div>
+      </div>
+      <div class="card__actions">
+        <button class="card__btn">
+          <img class="card__icon" src="./icons/heart.svg" alt="">&nbsp;Wishlist
+        </button>
+        <button class="card__btn card__btn--primary">
+          <img class="card__icon" src="./icons/shopping-bag.svg" alt="">&nbsp;Add to cart
+        </button>
+      </div>
+    `;
+
+    this.element = card;
+  }
+
+  destroy() {
+    this.element = null;
   }
 }
